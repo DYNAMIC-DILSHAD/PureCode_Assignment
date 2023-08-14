f78b3a6b7341a71e6c26eb6c03e30a41019ae1ad
Dilshad  Ansari <107291715+DYNAMIC-DILSHAD@users.noreply.github.com>
2023-08-14 11:48:24 +0530

    Update Cards.js

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,14 +1,14 @@
 import React from 'react'
-// import DILIMG from './Asset/DILIMG.jpg'
 import { AiOutlineBars } from 'react-icons/ai'
 import { MdOutlineMessage } from 'react-icons/md'
-import { BiEditAlt,BiSolidStopwatch } from 'react-icons/bi'
+import { BiEditAlt, BiSolidStopwatch } from 'react-icons/bi'
 
 
 const Cards = ({ name }) => {
 
 	return (
 
+		// here we wrote code for making card 
 		<div className='card_container'>
 			<div className='cards'>
 				<div className='title'>
@@ -17,17 +17,17 @@ const Cards = ({ name }) => {
 
 					<div className='time'>
 						<p>01:33/09:30</p>
-						<BiSolidStopwatch className='stopWatch'/>
+						<BiSolidStopwatch className='stopWatch' />
 						<p>june 06</p>
 					</div>
 
 				</div>
 				<div className='card_para'>
-				<p>In publishing and graphic design, Lorem ipsum is
-					a placeholder text commonly used to demonstrate the visual form of a document or a typeface
-				</p>
+					<p>In publishing and graphic design, Lorem ipsum is
+						a placeholder text commonly used to demonstrate the visual form of a document or a typeface
+					</p>
 				</div>
-				
+
 				<div className='copmlete'>
 
 					<p>T-Wind</p>
@@ -42,18 +42,18 @@ const Cards = ({ name }) => {
 
 				<div className='image_footer'>
 					<div className='img_footer'>
-						<img src='DILIMGS.jfif' className='image' alt=''/>
-						<img src='DILIMGS.jfif' className='image' alt=''/>
+						<img src='DILIMGS.jfif' className='image' alt='' />
+						<img src='DILIMGS.jfif' className='image' alt='' />
 						<img src='DILIMGS.jfif' className='image' alt='' />
 
 					</div>
 					<div className='icon_ footer'>
 
-							<AiOutlineBars />
-							<span>15/100</span>
-							<MdOutlineMessage />
-							<span>3</span>
-							<BiEditAlt />
+						<AiOutlineBars />
+						<span>15/100</span>
+						<MdOutlineMessage />
+						<span>3</span>
+						<BiEditAlt />
 					</div>
 
 				</div>
