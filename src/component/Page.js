8c184f45f75030679b4d7ef09bb96ca58a56d998
Dilshad  Ansari <107291715+DYNAMIC-DILSHAD@users.noreply.github.com>
2023-08-14 11:49:08 +0530

    Update Page.js

--- src/component/Page.js
+++ src/component/Page.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
-// style={({ isActive }) => ({
-//   color: isActive ? '#fff' : '#545e6f',
-//   background: isActive ? '#7600dc' : '#f0f0f0',
-// })}
+
 const Page = () => {
   return (
-	<div className='anc_tag'>
-    <NavLink to="/" className='tag' >Profile</NavLink>
-    <NavLink to="/" className='tag'>Project</NavLink>
-    <NavLink to="/" className='tag'>Post</NavLink>
-    <NavLink to="/" className='tag'>Setting</NavLink>
-  </div>
+      // it is nav link for (Profile,project,post,setting) 
+    <div className='anc_tag'>
+      <NavLink to="/" className='tag' >Profile</NavLink>
+      <NavLink to="/" className='tag'>Project</NavLink>
+      <NavLink to="/" className='tag'>Post</NavLink>
+      <NavLink to="/" className='tag'>Setting</NavLink>
+    </div>
   )
 }
 
