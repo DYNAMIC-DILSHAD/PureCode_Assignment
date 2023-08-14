216959013c8025eda2b5200a0837d560db07d585
Dilshad  Ansari <107291715+DYNAMIC-DILSHAD@users.noreply.github.com>
2023-08-14 11:51:36 +0530

    Update Pages.js

--- src/component/Pages.js
+++ src/component/Pages.js
@@ -1,28 +1,26 @@
 import React from 'react'
 
 const Pages = () => {
-  return (
-	<div className='main_container'>
-		<div className='container'>
-		<div className='pages'>
-			Profile
-			<div>
-				<span>T-wind /</span>
-				<span>Pages /</span>
-				<span>profile</span>
-			</div>
+	return (
+		<div className='main_container'>
+			<div className='container'>
+				<div className='pages'>
+					Profile
+					<div>
+						<span>T-wind /</span>
+						<span>Pages /</span>
+						<span>profile</span>
+					</div>
+				</div>
 
+				<div className='fst_button'>
+					<button className='button'>CREATE NEW</button>
+				</div>
+
+			</div>
 			
 		</div>
-		<div className='fst_button'>
-			<button className='button'>CREATE NEW</button>
-		</div>
-
-		</div>
-		
-
-	</div>
-  )
+	)
 }
 
 export default Pages
